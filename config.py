6a4327b00b787bf203e514e37b546a84fb2e918f
zurgenius <96647976+zurgenius@users.noreply.github.com>
2025-07-10 07:14:10 +0300

    changed config to use env vars

--- config.py
+++ config.py
@@ -1,5 +1,11 @@
+import os
+
+bot_token = os.environ.get('BOT_TOKEN')
+database_url = os.environ.get('DATABASE_URL')
+
+
 def load_config():
     return {
-        "bot_token": '',
-        "DATABASE_URL": 'sqlite:///user_data.db'
+        "bot_token": bot_token,
+        "DATABASE_URL": database_url
     }
