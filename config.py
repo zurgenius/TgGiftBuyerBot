b19a7513c1a2e6e8349a5a99883c1c95db9aad6e
zurgenius <96647976+zurgenius@users.noreply.github.com>
2025-07-10 07:34:26 +0300

    added loadenv

--- config.py
+++ config.py
@@ -1,7 +1,11 @@
 import os
+from dotenv import load_dotenv
+
+load_dotenv()  # Загружает переменные из .env
 
 bot_token = os.environ.get('BOT_TOKEN')
 database_url = os.environ.get('DATABASE_URL')
+
 
 
 def load_config():
